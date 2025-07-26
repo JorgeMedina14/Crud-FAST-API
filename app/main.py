d0b9c2a7bdfb3b0f01b7659575c37586a93f8d69
ALejo20032023 <jmorales.ipedep@gmail.com>
2025-07-26 00:16:59 -0600

    subiendo cambios

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from app.api.routes import items
 
 app = FastAPI(
     title="Mini API CRUD",
-    description="Una API CRUD simple para gestionar items en memoria",
+    description="Una API CRUD simple para gestionar items en sin base de datos en memoria con python y fastapi",
     version="1.0.0"
 )
 
-# Incluir las rutas
+# Incluir rutas
 app.include_router(items.router, prefix="/api/v1", tags=["items"])
 
 @app.get("/")
